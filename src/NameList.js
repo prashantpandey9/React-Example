b5d00ca9663ceac1e8c7292d647db6e7f175de3f
prashantpandey9 <prashantpandey94551@gmail.com>
2020-05-21 12:08:29 +0530

    List&Keys

--- src/NameList.js
+++ src/NameList.js
@@ -1,35 +1,35 @@
 import React from 'react'
 import Person from './Person'
 export default function NameList() {
-    // const names=['Bruce','Clark','Diana']
-    const persons=[
-        {
-            id:1,
-            name:'Bruce',
-            age:30,
-            skill:'react'
-        },
-        {
-            id:1,
-            name:'Clark',
-            age:30,
-            skill:'react'
-        },
-        {
-            id:1,
-            name:'Diana',
-            age:30,
-            skill:'react'
-        }
-    ]
-    // const nameList=names.map(name=><h2>WElcome    {name}</h2>)
-    const personList=persons.map(person=>(
-        <Person person={person}/>
-    ))
+    const names=['Bruce','Clark','Diana']
+    // const persons=[
+    //     {
+    //         id:1,
+    //         name:'Bruce',
+    //         age:30,
+    //         skill:'react'
+    //     },
+    //     {
+    //         id:2,
+    //         name:'Clark',
+    //         age:30,
+    //         skill:'react'
+    //     },
+    //     {
+    //         id:3,
+    //         name:'Diana',
+    //         age:30,
+    //         skill:'react'
+    //     }
+    // ]
+    const nameList=names.map((name,index)=><h2 key={index}>{index} WElcome {name}</h2>)
+    // const personList=persons.map(person=>(
+    //     <Person key={person.id} person={person}/>
+    // ))
     return (
         <div>
-            {/* {nameList} */}
-            {personList}
+            {nameList}
+             {/* {personList} */}
             
         </div>
     )
